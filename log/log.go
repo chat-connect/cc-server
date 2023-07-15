6b68112e329b68c266390fffec9d1929130414c5
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-15 10:40:51 +0900

    ログ設定に新規作成のフラグを追加

--- log/log.go
+++ log/log.go
@@ -5,7 +5,7 @@ import (
 )
 
 func GeneratApiLog() (fp *os.File) {
-	fp, err := os.OpenFile("log/api_debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	fp, err := os.OpenFile("log/api_debug.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
@@ -14,7 +14,7 @@ func GeneratApiLog() (fp *os.File) {
 }
 
 func GeneratBatchLog() (fp *os.File) {
-	fp, err := os.OpenFile("log/batch_debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	fp, err := os.OpenFile("log/batch_debug.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
