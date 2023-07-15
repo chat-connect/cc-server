85f75e7781c6999e33c5c2cdea0c7a9109f662d7
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-15 11:27:42 +0900

    テスト用のマイグレーションを追加

---
+++ docs/sql/migration/20230715_insert_user.sql
@@ -0,0 +1,18 @@
+-- テストユーザー
+INSERT INTO `user` (
+    `user_key`,
+    `username`,
+    `email`,
+    `password`,
+    `status`,
+    `created_at`,
+    `updated_at`
+) VALUES (
+    "-uTSuGtw_1Eq",
+    "test",
+    "test@gmail.com",
+    "$2a$10$5zvv6gnZF9tBs8S38oaw5e7W/Oqw9YpeXWynx.EV2/myKU0hOV5g2",
+    "offline",
+    "2023-07-15 02:17:37",
+    "2023-07-15 02:17:37"
+);
