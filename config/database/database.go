9773678ef9f29fd788457d35325f454c7e05b454
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-23 01:48:07 +0900

    テストdbの接続処理をtestディレクトリに移動

--- config/database/database.go
+++ config/database/database.go
@@ -3,13 +3,12 @@ package database
 import (
 	"fmt"
 	"os"
-	"database/sql"
 	
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func NewGormDB() (db *gorm.DB) {
+func NewDB() (db *gorm.DB) {
 	user := os.Getenv("MYSQL_USER")
 	password := os.Getenv("MYSQL_PASSWORD")
 	host := os.Getenv("MYSQL_HOST")
@@ -20,19 +19,6 @@ func NewGormDB() (db *gorm.DB) {
 	}
 
 	db.SingularTable(true)
-
-	return db
-}
-
-func NewSqlDB() (db *sql.DB) {
-	user := os.Getenv("MYSQL_USER")
-	password := os.Getenv("MYSQL_PASSWORD")
-	host := os.Getenv("MYSQL_HOST")
-	name := os.Getenv("MYSQL_DATABASE")
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, password, host, name))
-	if err != nil {
-		panic(err)
-	}
 
 	return db
 }
