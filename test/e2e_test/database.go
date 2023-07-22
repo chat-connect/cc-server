c470f37e8bf46bd16561d3176877cbbb490ddff7
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-23 01:49:18 +0900

    dbのテストデータをsqlファイルから取得するように修正

--- test/e2e_test/database.go
+++ test/e2e_test/database.go
@@ -1,43 +1,29 @@
 package e2e_test
 
 import (
+	"fmt"
+	"os"
 	"io/ioutil"
-
-	"github.com/jinzhu/gorm"
-
-	"github.com/chat-connect/cc-server/config/database"
+	"database/sql"
 )
-
-type Model interface{}
 
 type File interface{}
 
-func SetupTestDatabase(models ...Model) (db *gorm.DB) {
-	db = database.NewGormDB()
-	for _, model := range models {
-		err := db.AutoMigrate(model).Error
-		if err != nil {
-			panic(err)
-		}
+func NewTestDB() (db *sql.DB) {
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	name := os.Getenv("MYSQL_DATABASE")
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, password, host, name))
+	if err != nil {
+		panic(err)
 	}
 
 	return db
 }
 
-func TeardownTestDatabase(db *gorm.DB, models ...Model) {
-	for _, model := range models {
-		err := db.DropTableIfExists(model).Error
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	db.Close()
-}
-
-func LoadTestData(files ...File) {
-	db := database.NewSqlDB()
-    defer db.Close()
+func LoadTestSql(files ...File) (db *sql.DB) {
+	db = NewTestDB()
 	
 	for _, file := range files {
 		query, err := ioutil.ReadFile(file.(string))
@@ -47,4 +33,32 @@ func LoadTestData(files ...File) {
 
 		_, err = db.Exec(string(query))
 	}
+
+	return db
+}
+
+func ClearTestSql(db *sql.DB) {
+    _, err := db.Exec("SHOW TABLES")
+    if err != nil {
+        panic(err.Error())
+    }
+
+    rows, err := db.Query("SHOW TABLES")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer rows.Close()
+
+    var tableName string
+    for rows.Next() {
+        if err := rows.Scan(&tableName); err != nil {
+            panic(err.Error())
+        }
+        _, err = db.Exec("DROP TABLE " + tableName)
+        if err != nil {
+            panic(err.Error())
+        }
+    }
+
+    db.Close()
 }
