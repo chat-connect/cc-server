49a8a3e9723c0d5105da7406959d3a8795ca73b6
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-22 17:02:36 +0900

    ユーザー登録を検証するe2eテスト用を追加

---
+++ test/e2e_test/user_test.go
@@ -0,0 +1,89 @@
+package e2e_test
+
+import (
+	"fmt"
+	"os"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/chat-connect/cc-server/api/presentation/parameter"
+	"github.com/chat-connect/cc-server/api/presentation/output"
+	"github.com/chat-connect/cc-server/domain/model"
+)
+
+func TestUserRegister(t *testing.T) {
+	models := []Model{
+		&model.User{},
+	}
+
+	db := SetupTestDatabase(t, models...)
+	defer TeardownTestDatabase(t, db, models...)
+
+	testCases := []struct {
+		name         string
+		body  *parameter.UserRegister
+		expectedCode int
+		expectedKey  string
+	}{
+		{
+			name: "Successful User Register",
+			body: &parameter.UserRegister{
+				Username:  "test",
+				Email:     "test@example.com",
+				Password:  "test_password",
+			},
+			expectedCode: http.StatusOK,
+			expectedKey:  "pRxN4QA9bt4p",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			jsonData, err := json.Marshal(tc.body)
+			if err != nil {
+				fmt.Println("JSON encoding error:", err)
+				return
+			}
+
+			req, err := http.NewRequest("POST", fmt.Sprintf("%s/auth/user_register", os.Getenv("TEST_API_URL")), bytes.NewBuffer(jsonData))
+			if err != nil {
+				t.Fatalf("Failed to create request: %v", err)
+			}
+
+			req.Header.Set("Content-Type", "application/json")
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatalf("Failed to send request: %v", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tc.expectedCode {
+				t.Fatalf("Expected status code %v, but got %v", tc.expectedCode, resp.StatusCode)
+			}
+
+			if tc.expectedCode == http.StatusOK {
+				actual := &output.UserRegister{}
+				expect := &output.UserRegister{
+					UserKey:  "pRxN4QA9bt4p",
+					Username: "test",
+					Email:    "test@example.com",
+					Message:  "user register completed",
+				}
+
+				err = json.NewDecoder(resp.Body).Decode(actual)
+				if err != nil {
+					t.Fatalf("Failed to parse response: %v", err)
+				}
+
+				actual.UserKey = "pRxN4QA9bt4p"
+				
+				assert.Equal(t, expect, actual)
+			}
+		})
+	}
+}
