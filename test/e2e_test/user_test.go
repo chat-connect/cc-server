152457f5d6e0b8be5723d149f1d46b7c10aaa728
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-22 18:48:01 +0900

    ユーザーログインを検証するe2eテスト用を追加

--- test/e2e_test/user_test.go
+++ test/e2e_test/user_test.go
@@ -21,12 +21,12 @@ func TestUserRegister(t *testing.T) {
 		&model.User{},
 	}
 
-	db := SetupTestDatabase(t, models...)
-	defer TeardownTestDatabase(t, db, models...)
+	db := SetupTestDatabase(models...)
+	defer TeardownTestDatabase(db, models...)
 
 	testCases := []struct {
 		name         string
-		body  *parameter.UserRegister
+		body         *parameter.UserRegister
 		expectedCode int
 		expectedKey  string
 	}{
@@ -81,6 +81,80 @@ func TestUserRegister(t *testing.T) {
 				}
 
 				actual.UserKey = "pRxN4QA9bt4p"
+				
+				assert.Equal(t, expect, actual)
+			}
+		})
+	}
+}
+
+func TestUserLogin(t *testing.T) {
+	models := []Model{
+		&model.User{},
+	}
+
+	files := []File{
+		"sql/user/user.sql",
+	}
+
+	db := SetupTestDatabase(models...)
+	LoadTestData(files...)
+	defer TeardownTestDatabase(db, models...)
+
+	testCases := []struct {
+		name         string
+		body         *parameter.UserLogin
+		expectedCode int
+	}{
+		{
+			name: "Successful User Register",
+			body: &parameter.UserLogin{
+				Email:     "test@example.com",
+				Password:  "test",
+			},
+			expectedCode: http.StatusOK,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			jsonData, err := json.Marshal(tc.body)
+			if err != nil {
+				fmt.Println("JSON encoding error:", err)
+				return
+			}
+
+			req, err := http.NewRequest("POST", fmt.Sprintf("%s/auth/user_login", os.Getenv("TEST_API_URL")), bytes.NewBuffer(jsonData))
+			if err != nil {
+				t.Fatalf("Failed to create request: %v", err)
+			}
+
+			req.Header.Set("Content-Type", "application/json")
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatalf("Failed to send request: %v", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tc.expectedCode {
+				t.Fatalf("Expected status code %v, but got %v", tc.expectedCode, resp.StatusCode)
+			}
+
+			if tc.expectedCode == http.StatusOK {
+				actual := &output.UserLogin{}
+				expect := &output.UserLogin{
+					UserKey:  "pRxN4QA9bt4p",
+					Username: "test",
+					Email:    "test@example.com",
+					Message:  "user login completed",
+				}
+
+				err = json.NewDecoder(resp.Body).Decode(actual)
+				if err != nil {
+					t.Fatalf("Failed to parse response: %v", err)
+				}
+
+				expect.Token = actual.Token
 				
 				assert.Equal(t, expect, actual)
 			}
