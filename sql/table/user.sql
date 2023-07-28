4ff637b7fbe97b40d1341508afe4c6a37a7161b7
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-28 19:06:01 +0900

    ユニークキーとインデックスを追加

--- sql/table/user.sql
+++ sql/table/user.sql
@@ -1,7 +1,7 @@
 -- ユーザー情報
 CREATE TABLE `user` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT "ユーザーID",
-    `user_key` VARCHAR(12) NOT NULL UNIQUE COMMENT "ユーザーKey",
+    `user_key` VARCHAR(12) NOT NULL UNIQUE KEY COMMENT "ユーザーKey",
     `username` VARCHAR(50) NOT NULL COMMENT "ユーザー名",
     `email` VARCHAR(255) NOT NULL COMMENT "メールアドレス",
     `password` VARCHAR(255) NOT NULL COMMENT "パスワード",
@@ -10,6 +10,4 @@ CREATE TABLE `user` (
     `created_at` TIMESTAMP NOT NULL COMMENT "作成日時",
     `updated_at` TIMESTAMP NOT NULL COMMENT "更新日時",
     PRIMARY KEY (`id`)
-    UNIQUE KEY `UK_user_key` (`user_key`)
-    KEY `IDX_user_key` (`user_key`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
