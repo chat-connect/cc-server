0f32ce0c268dd2037b62e8ed3feffe104753383e
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-29 22:17:37 +0900

    RoomUserモデルを追加

---
+++ domain/model/roomUser.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+    "time"
+)
+
+type RoomUsers []RoomUser
+
+type RoomUser struct {
+	ID          int64     `json:"id"`
+	RoomUserKey string    `json:"room_user_key"`
+	RoomID      int64     `json:"room_id"`
+	UserID      int64     `json:"user_id"`
+ 	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
+    UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+func EmptyRoomUser() *RoomUser {
+	return &RoomUser{}
+}
+
+func (roomUser *RoomUser) IsEmpty() bool {
+	return (
+		roomUser.ID == 0 &&
+		roomUser.RoomUserKey == "" &&
+		roomUser.RoomID == 0 &&
+		roomUser.UserID == 0 &&
+		roomUser.Status == "")
+}
