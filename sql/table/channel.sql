065ee9be996bda3ce50886b8bfe1e2d60b06f6f4
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-06 00:18:26 +0900

    channelテーブルを追加

---
+++ sql/table/channel.sql
@@ -0,0 +1,12 @@
+-- チャット情報
+CREATE TABLE `channel` (
+    `id`          BIGINT       NOT NULL AUTO_INCREMENT COMMENT "チャンネルID",
+    `channel_key` VARCHAR(20)  NOT NULL UNIQUE KEY     COMMENT "チャンネルKEY",
+    `room_key`    VARCHAR(20)  NOT NULL                COMMENT "ルームKEY",
+    `name`        VARCHAR(50)  NOT NULL                COMMENT "チャンネル名",
+    `explanation` VARCHAR(191) NOT NULL                COMMENT "説明",
+    `type`        VARCHAR(191) NOT NULL                COMMENT "種別",
+    `created_at`  TIMESTAMP    NOT NULL                COMMENT "作成日時",
+    `updated_at`  TIMESTAMP    NOT NULL                COMMENT "更新日時",
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
