ad1979810c37341e3f4dbbd449e290f8e0168f67
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-05 16:05:31 +0900

    Update SQL

--- sql/table/room.sql
+++ sql/table/room.sql
@@ -1,14 +1,14 @@
 -- ルーム情報
 CREATE TABLE `room` (
-    `id` INT NOT NULL AUTO_INCREMENT COMMENT "ルームID",
-    `room_key` VARCHAR(20) NOT NULL UNIQUE KEY COMMENT "ルームKEY",
-    `user_key` VARCHAR(20) NOT NULL COMMENT "ユーザーKEY",
-    `name` VARCHAR(50) NOT NULL COMMENT "ルーム名",
-    `explanation` VARCHAR(50) NOT NULL COMMENT "説明",
-    `image_path` VARCHAR(50) NOT NULL COMMENT "画像パス",
-    `user_count` INT NOT NULL COMMENT "ユーザー数",
-    `status` VARCHAR(255) NOT NULL COMMENT "状態",
-    `created_at` TIMESTAMP NOT NULL COMMENT "作成日時",
-    `updated_at` TIMESTAMP NOT NULL COMMENT "更新日時",
+    `id`          BIGINT       NOT NULL AUTO_INCREMENT COMMENT "ルームID",
+    `room_key`    VARCHAR(20)  NOT NULL UNIQUE KEY     COMMENT "ルームKEY",
+    `user_key`    VARCHAR(20)  NOT NULL                COMMENT "ユーザーKEY",
+    `name`        VARCHAR(50)  NOT NULL                COMMENT "ルーム名",
+    `explanation` VARCHAR(50)  NOT NULL                COMMENT "説明",
+    `image_path`  VARCHAR(50)  NOT NULL                COMMENT "画像パス",
+    `user_count`  INT          NOT NULL                COMMENT "ユーザー数",
+    `status`      VARCHAR(191) NOT NULL                COMMENT "状態",
+    `created_at`  TIMESTAMP    NOT NULL                COMMENT "作成日時",
+    `updated_at`  TIMESTAMP    NOT NULL                COMMENT "更新日時",
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- test/e2e_test/sql/user/user_table.sql
+++ test/e2e_test/sql/user/user_table.sql
@@ -1,13 +1,13 @@
 -- ユーザー情報
 CREATE TABLE `user` (
-    `id` INT NOT NULL AUTO_INCREMENT COMMENT "ユーザーID",
-    `user_key` VARCHAR(20) NOT NULL UNIQUE KEY COMMENT "ユーザーKEY",
-    `name` VARCHAR(50) NOT NULL COMMENT "ユーザー名",
-    `email` VARCHAR(255) NOT NULL COMMENT "メールアドレス",
-    `password` VARCHAR(255) NOT NULL COMMENT "パスワード",
-    `token` VARCHAR(255) NOT NULL COMMENT "アクセストークン",
-    `status` VARCHAR(255) NOT NULL COMMENT "状態",
-    `created_at` TIMESTAMP NOT NULL COMMENT "作成日時",
-    `updated_at` TIMESTAMP NOT NULL COMMENT "更新日時",
+    `id`         BIGINT       NOT NULL AUTO_INCREMENT COMMENT "ユーザーID",
+    `user_key`   VARCHAR(20)  NOT NULL UNIQUE KEY     COMMENT "ユーザーKEY",
+    `name`       VARCHAR(50)  NOT NULL                COMMENT "ユーザー名",
+    `email`      VARCHAR(191) NOT NULL                COMMENT "メールアドレス",
+    `password`   VARCHAR(191) NOT NULL                COMMENT "パスワード",
+    `token`      VARCHAR(255) NOT NULL                COMMENT "アクセストークン",
+    `status`     VARCHAR(191) NOT NULL                COMMENT "状態",
+    `created_at` TIMESTAMP    NOT NULL                COMMENT "作成日時",
+    `updated_at` TIMESTAMP    NOT NULL                COMMENT "更新日時",
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
