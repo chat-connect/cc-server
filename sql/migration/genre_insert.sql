edfa52995dab4012b729d9fcc7f1dc904391c4d2
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-16 12:16:32 +0900

    genreに項目を追加

--- sql/migration/genre_insert.sql
+++ sql/migration/genre_insert.sql
@@ -3,4 +3,5 @@ INSERT INTO genre (genre_key, name, description, type, created_at, updated_at) V
 ('pRxN4QA9bt42', 'FPS', 'FPS', 'FPS', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
 ('pRxN4QA9bt43', 'Sound', 'Sound', 'Sound', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
 ('pRxN4QA9bt44', 'Action', 'Action', 'Action', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
-('pRxN4QA9bt45', 'Horror', 'Horror', 'Horror', '2023-07-22 07:40:31', '2023-07-22 07:40:31');
+('pRxN4QA9bt45', 'Horror', 'Horror', 'Horror', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
+('pRxN4QA9bt46', 'Other', 'Other', 'Other', '2023-07-22 07:40:31', '2023-07-22 07:40:31');
