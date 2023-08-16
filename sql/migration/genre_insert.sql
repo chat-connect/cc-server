bdb7635eb1876f3f74bf8c63625a7693444d7f42
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-16 11:47:56 +0900

    Create genre_insert.sql

---
+++ sql/migration/genre_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO genre (genre_key, name, description, type, created_at, updated_at) VALUES
+('pRxN4QA9bt41', 'RPG', 'RPG', 'RPG', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
+('pRxN4QA9bt42', 'FPS', 'FPS', 'FPS', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
+('pRxN4QA9bt43', 'Sound', 'Sound', 'Sound', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
+('pRxN4QA9bt44', 'Action', 'Action', 'Action', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
+('pRxN4QA9bt45', 'Horror', 'Horror', 'Horror', '2023-07-22 07:40:31', '2023-07-22 07:40:31');
