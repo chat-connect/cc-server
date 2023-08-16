b00f8d3fae8f8e50766d926ca4e70233f6d35cdc
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-16 11:37:07 +0900

    migrationを修正

--- sql/migration/20230715_insert_user.sql
+++ sql/migration/20230715_insert_user.sql
@@ -1,20 +1,3 @@
 -- テストユーザー
-INSERT INTO `user` (
-    `user_key`,
-    `username`,
-    `email`,
-    `password`,
-    `token`,
-    `status`,
-    `created_at`,
-    `updated_at`
-) VALUES (
-    "-uTSuGtw_1Eq",
-    "test",
-    "test@gmail.com",
-    "$2a$10$5zvv6gnZF9tBs8S38oaw5e7W/Oqw9YpeXWynx.EV2/myKU0hOV5g2",
-    "",
-    "offline",
-    "2023-07-15 02:17:37",
-    "2023-07-15 02:17:37"
-);
+INSERT INTO user (user_key, username, email, password, token, status, created_at, updated_at) VALUES 
+("-uTSuGtw_1Eq", "test", "test@gmail.com", "$2a$10$5zvv6gnZF9tBs8S38oaw5e7W/Oqw9YpeXWynx.EV2/myKU0hOV5g2", "", "logout", "2023-07-15 02:17:37", "2023-07-15 02:17:37");
