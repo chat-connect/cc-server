d68f34ebdb9b038baa62ca8368845337ad5cf4e8
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-16 13:29:07 +0900

    Update sql

--- sql/migration/game_insert.sql
+++ sql/migration/game_insert.sql
@@ -1,4 +1,5 @@
 INSERT INTO game (game_key, genre_key, name, description, type, created_at, updated_at) VALUES
+('pRxN4QA9bt40', 'pRxN4QA9bt40', 'None', 'None', 'None', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
 ('pRxN4QA9bt41', 'pRxN4QA9bt41', 'game1', 'game1', 'game1', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
 ('pRxN4QA9bt42', 'pRxN4QA9bt42', 'game2', 'game2', 'game2', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
 ('pRxN4QA9bt43', 'pRxN4QA9bt43', 'game3', 'game3', 'game3', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
