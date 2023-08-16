c49d0e81a0640ea9bbfb35a42e1d0ce18b198468
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-16 11:40:02 +0900

    Create user_insert.sql

---
+++ sql/migration/user_insert.sql
@@ -0,0 +1,4 @@
+-- テストユーザー
+INSERT INTO user (user_key, name, email, password, token, status, image_path, created_at, updated_at) VALUES
+('pRxN4QA9bt4p', 'test', 'test@example.com', '$2a$10$V/tLtSH8zGeVemRNMbru6eXlErp14Cqo6rRI1.YmxS82.vrolr2B6', 'nil', 'logout', '/user/default_icon.png', '2023-07-22 07:40:31', '2023-07-22 07:40:31');
+
