f6f63710547db3e1f929bc893e11f6e1e0ba3808
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-18 15:59:04 +0900

    Create room_user.sql

---
+++ sql/table/api/room_user.sql
@@ -0,0 +1,12 @@
+-- ルームのユーザー情報
+CREATE TABLE `room_user` (
+    `id`            BIGINT       NOT NULL AUTO_INCREMENT COMMENT "ルームユーザーID",
+    `room_user_key` VARCHAR(20)  NOT NULL UNIQUE KEY     COMMENT "ルームユーザーKEY",
+    `room_key`      VARCHAR(20)  NOT NULL                COMMENT "ルームKEY",
+    `user_key`      VARCHAR(20)  NOT NULL                COMMENT "ユーザーKEY",
+    `host`          TINYINT      NOT NULL DEFAULT "0"    COMMENT "ホストユーザー",
+    `status`        VARCHAR(191) NOT NULL                COMMENT "状態",
+    `created_at`    TIMESTAMP    NOT NULL                COMMENT "作成日時",
+    `updated_at`    TIMESTAMP    NOT NULL                COMMENT "更新日時",
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
