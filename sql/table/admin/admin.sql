3a51a9a7394e31fc5771e6670a6e95b9e5cb04a6
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-18 23:24:43 +0900

    Update admin.sql

--- sql/table/admin/admin.sql
+++ sql/table/admin/admin.sql
@@ -3,7 +3,6 @@ CREATE TABLE `admin` (
     `id`               BIGINT       NOT NULL AUTO_INCREMENT COMMENT "企業ID",
     `admin_key`        VARCHAR(20)  NOT NULL UNIQUE KEY     COMMENT "企業KEY",
     `admin_api_key`    VARCHAR(20)  NOT NULL UNIQUE KEY     COMMENT "企業APIKEY",    
-    `admin_secret_key` VARCHAR(20)  NOT NULL UNIQUE KEY     COMMENT "企業シークレットKEY",
     `admin_name`       VARCHAR(50)  NOT NULL                COMMENT "企業名",
     `game_title`       VARCHAR(50)  NOT NULL                COMMENT "ゲームタイトル",
     `game_image_path`  VARCHAR(191) NOT NULL                COMMENT "画像パス",
