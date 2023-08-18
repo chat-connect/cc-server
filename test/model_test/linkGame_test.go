1d36f7cfa1d5c91f23525973aaa93138e65ee3ab
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-19 01:37:58 +0900

    LinkGameモデルを追加

---
+++ test/model_test/linkGame_test.go
@@ -0,0 +1,41 @@
+package model_test
+
+import (
+	"testing"
+
+	"github.com/game-connect/gc-server/domain/model"
+)
+
+func TestLinkGameModel_EmptyLinkGame(t *testing.T) {
+	tests := []struct {
+		name     string
+		linkGame *model.LinkGame
+		expected bool
+	}{
+		{
+			name:     "Empty LinkGame",
+			linkGame: model.EmptyLinkGame(),
+			expected: true,
+		},
+		{
+			name: "Not Empty LinkGame",
+			linkGame: &model.LinkGame{
+				ID:            1,
+				LinkGameKey:   "test_key",
+				ApiKey:        "test_api_key",
+				GameTitle:     "test_game_title",
+				GameImagePath: "test_image_path",
+				GameGenre:     "test_genre",
+			},
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if empty := test.linkGame.IsEmpty(); empty != test.expected {
+				t.Errorf("Expected IsEmpty() to return %v, but got %v", test.expected, empty)
+			}
+		})
+	}
+}
