3a38bb19b46d26e10ebbe2ffa21ca2508a2f4680
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-19 19:59:59 +0900

    スペースを追加

--- game/presentation/parameter/user.go
+++ game/presentation/parameter/user.go
@@ -2,7 +2,7 @@ package parameter
 
 type LoginUser struct {
 	LinkGameKey string `json:"link_game_key"`
-	ApiKey   string `json:"api_key"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ApiKey      string `json:"api_key"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
 }
