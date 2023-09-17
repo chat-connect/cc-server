731cf632ee617ea03b6cef51ed02d9f39e1f40cc
Nishiumi Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-09-17 21:39:36 +0900

    Update game_insert.sql

--- docs/sql/migration/game_insert.sql
+++ docs/sql/migration/game_insert.sql
@@ -1,8 +1,2 @@
-INSERT INTO game (game_key, genre_key, name, description, type, created_at, updated_at) VALUES
-('pRxN4QA9bt40', 'pRxN4QA9bt40', 'None', 'None', 'None', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
-('pRxN4QA9bt41', 'pRxN4QA9bt41', 'game1', 'game1', 'game1', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
-('pRxN4QA9bt42', 'pRxN4QA9bt42', 'game2', 'game2', 'game2', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
-('pRxN4QA9bt43', 'pRxN4QA9bt43', 'game3', 'game3', 'game3', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
-('pRxN4QA9bt44', 'pRxN4QA9bt44', 'game4', 'game4', 'game4', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
-('pRxN4QA9bt45', 'pRxN4QA9bt45', 'game5', 'game5', 'game5', '2023-07-22 07:40:31', '2023-07-22 07:40:31'),
-('pRxN4QA9bt46', 'pRxN4QA9bt46', 'game6', 'game6', 'game6', '2023-07-22 07:40:31', '2023-07-22 07:40:31');
+INSERT INTO game (game_key, genre_key, admin_user_key, api_key, game_title, game_image_path, created_at, updated_at) VALUES
+('None', 'None', 'None', 'None', 'None', 'None', '2023-07-22 07:40:31', '2023-07-22 07:40:31');
